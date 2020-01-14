889e7b5c750488195948bc46b65fb09e2e28de93
adekanyetomie <adekanyetomy@gmail.com>
2020-01-14 16:55:25 +0100

    buttons are working

---
+++ src/components/control.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Controls =  ({paused, onPausedToggle}) => 
+    <div className="field is-grouped is-grouped-centered">
+        <p className="control">
+            <button className="button is-danger is-outlined is-rounded is-medium"disabled= {paused} onClick={onPausedToggle} >
+                 Pause
+            </button>
+        </p>
+        <p className="control">
+            <button className="button is-warning is-outlined is-rounded is-medium" disabled= {!paused} onClick= {onPausedToggle}>
+                Resume
+            </button>
+        </p>
+    </div>
+
+export default Controls
--- src/components/countdown.js
+++ src/components/countdown.js
@@ -1,21 +1,28 @@
 import React, {Component} from 'react'
 import moment from 'moment'
+import Controls  from './control'
 
 
 class Countdown extends Component{
 
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
 
         this.state = {
             
-            duration: this.getTimeLeft()
+            duration: this.getTimeLeft(),
+            paused: false
         }
+        this.togglePaused = this.togglePaused.bind(this)
     
     }
     componentDidMount(){
         this.interval = setInterval(() => {this.setState({duration: this.getTimeLeft() })}, 1000)
-  }
+    }
+    componentWillUnmount(){
+        clearInterval(this.interval)
+    }
+
     getTimeLeft(){
         let now = moment(),
             newYear = moment({year: now.year()+1} ),
@@ -25,19 +32,37 @@ class Countdown extends Component{
         
 
     }
+    togglePaused(){
+       this.setState((prevState, props) => {
+
+            const paused = !prevState.paused
+            
+            if (paused){
+                clearInterval(this.interval)
+            }else{
+            this.interval = setInterval(() => {this.setState({duration: this.getTimeLeft() })}, 1000)
+    
+            }
+
+           return{
+                paused 
+           }
+       } )
+        
+    }
     render() 
     {
-        const {duration} = this.state
+        const {duration, paused} = this.state
 
-        return <section className="hero is-primary is-bold  is-fullheight  has-text-centered">
+        return <section className="hero is-dark is-bold  is-fullheight  has-text-centered">
             <div className="hero-body">
                 <div className="container">
                     <h1 className="title">
                         Count-down timer
                     </h1>
-                <div className="section">
+                <section className="section">
                 <nav className="level">
-                    <div className="level-item has-text-centered">
+                    <div className="level-item has-text-centered ">
                         <div>
                         <p className="heading">Days</p>
                         <p className="title">{Math.floor(duration.asDays())}</p>
@@ -63,8 +88,8 @@ class Countdown extends Component{
                     </div>
                 </nav>
 
-            </div>
-            
+            </section>
+            <Controls paused={paused} onPausedToggle={this.togglePaused} />
             </div>
         </div>
         </section>
